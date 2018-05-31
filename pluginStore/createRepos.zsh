6694760b34a8b3989a69bc5ffda4e8b7a10707ab
zxm <zxm@basecity.com>
2018-06-01 07:57:20 +0800

    add jdollar repo

--- pluginStore/createRepos.zsh
+++ pluginStore/createRepos.zsh
@@ -2,4 +2,4 @@
 
 PLUGIN_STORE_DIR=.
 
-python3 create_repository.py ../repo https://github.com/zhuangxm/plugin.video.overseaplayer.git:
+python3 create_repository.py ../repo https://github.com/zhuangxm/plugin.video.overseaplayer.git: https://github.com/jdollarKodi/repository.jdollar.git:repository
